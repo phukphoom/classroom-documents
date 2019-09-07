b75c7cf600bd300b60848f9ec1c2bcc1561220ad
EsitoneX <phoom0529@gmail.com>
2019-09-07 18:02:05 +0700

    7/9/2562 18:01 windows update [PF update GameLab Ass.4 completed]

--- Semester1-1/Programming fundamental/Assignment/W6_W7_W8_LabGame/LabGame/LabGame/src/main.cpp
+++ Semester1-1/Programming fundamental/Assignment/W6_W7_W8_LabGame/LabGame/LabGame/src/main.cpp
@@ -4,50 +4,76 @@
 
 using namespace std;
 
-void setwindow(int, int);
 void draw_ship(int, int);
+void erase_ship(int,int);
+bool over_frame(int, int);
 void gotoxy(int, int);
 
 int main() {
 
-	setwindow(1280,720);
-
 	char key = ' ';
 
-	int x = 38, y = 20;
+	int x = 75, y = 20;
 	draw_ship(x, y);
 	while (key != 'x') {
 		if (_kbhit()) {
 			key = _getch();
+			erase_ship(x, y);
 			if (key == 'a') {
-				draw_ship(--x, y);
+				if (!over_frame(x-1, y)) {
+					draw_ship(--x, y);
+				}
+				else {
+					draw_ship(x, y);
+				}
 			}
 			if (key == 'd') {
-				draw_ship(++x, y);
+				if (!over_frame(x+1, y)) {
+					draw_ship(++x, y);
+				}
+				else {
+					draw_ship(x, y);
+				}
 			}
 			if (key == 'w') {	
-				draw_ship(x, --y);
+				if (!over_frame(x, y-1)) {
+					draw_ship(x, --y);
+				}
+				else {
+					draw_ship(x, y);
+				}
 			}
 			if (key == 's') {	
-				draw_ship(x, ++y);
+				if (!over_frame(x, y+1)) {
+					draw_ship(x, ++y);
+				}
+				else {
+					draw_ship(x, y);
+				}
 			}
 			fflush(stdin);
 		}
+		Sleep(50);
 	}
 
 	return 0;
 }
-void setwindow(int width, int length) {
-	HWND console = GetConsoleWindow();
-	RECT r;
-	GetWindowRect(console, &r); 
 
-	MoveWindow(console, r.left, r.top, width, length, TRUE);
-}
 void draw_ship(int x, int y) {
-	system("cls");
 	gotoxy(x, y);
 	cout << "/-A-\\";
+}
+void erase_ship(int x, int y) {
+	gotoxy(x, y);
+	cout << "     ";
+}
+bool over_frame(int x, int y) {
+	if (x>=0&&x<=75 && y>=0&&y<=20) {
+		return false;
+	}
+	else {
+		return true;
+	}
 }
 void gotoxy(int x, int y) {
 	COORD c = { x,y };
