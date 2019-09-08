3d2b8b492894d63ce3fc07ce175d934377356255
EsitoneX <phoom0529@gmail.com>
2019-09-09 00:02:12 +0700

    9/9/2562 0:01 windows update [PF update GameLab Ass.4/2 cleaned code]

--- Semester1-1/Programming fundamental/Assignment/W6_W7_W8_LabGame/LabGame/LabGame/src/main.cpp
+++ Semester1-1/Programming fundamental/Assignment/W6_W7_W8_LabGame/LabGame/LabGame/src/main.cpp
@@ -3,7 +3,8 @@
 #include<conio.h>
 
 using namespace std;
-//----------------------------------------------- Function Prototype
+
+//----------------------------------------------- Function prototype -------------------------------------
 void draw_ship(int, int);
 void erase_ship(int,int);
 bool draw_bullet(bool, int, int);
@@ -13,7 +14,8 @@ bool over_frame(int, int);
 void gotoxy(int, int);
 void showcursor(bool);
 void setcolor(int, int);
-//-----------------------------------------------
+//----------------------------------------------- Function prototype -------------------------------------
+
 int main() {
 	showcursor(false);
 
@@ -21,25 +23,29 @@ int main() {
 	int ship_y;
 
 	const int maxbullet = 5;
-	bool bullet_shot[maxbullet] = {false};
+	bool bullet_shoot[maxbullet] = {false};
 	int bullet_x[maxbullet] = {0};
 	int bullet_y[maxbullet] = {0};
 	int numbullet = 0;
 	
+	int ship_startx = 38;
+	int ship_starty = 20;
 
-	ship_x = 38, ship_y = 20;
+	ship_x = ship_startx;
+	ship_y = ship_starty;
 	draw_ship(ship_x, ship_y);
 
 	char key = ' ';
 	while (key != 'x') {
-		//-----debug--bullet--------
+		//----------------------------------------------- debug bullet console ----------------------------
 		setcolor(0,7);
 		gotoxy(0, 22);
-		cout << "Bullet Status : " << bullet_shot[0] << "-" << bullet_shot[1] << "-" << bullet_shot[2] << "-" << bullet_shot[3] << "-" << bullet_shot[4];
-		//--------------------------
+		cout << "Bullet Status : " << bullet_shoot[0] << "-" << bullet_shoot[1] << "-";
+		cout << bullet_shoot[2] << "-" << bullet_shoot[3] << "-" << bullet_shoot[4];
+		//----------------------------------------------- debug bullet console ----------------------------
+		//----------------------------------------------- input block -------------------------------------
 		if (_kbhit()) {
 			key = _getch();
-			erase_ship(ship_x, ship_y);
 			if (key == 'a') {
 				if (!over_frame(ship_x-1, ship_y)) {
 					ship_x--;
@@ -61,35 +67,40 @@ int main() {
 				}
 			}
 			if (key == ' ') {
-				bullet_shot[numbullet] = true;
+				bullet_shoot[numbullet] = true;
 				bullet_x[numbullet] = ship_x;
 				bullet_y[numbullet] = ship_y-1;
 				
 				numbullet++;
-				if (numbullet >= maxbullet && bullet_shot[0]==false) {
+				if (numbullet >= maxbullet && bullet_shoot[0]==false) {
 					numbullet = 0;
 				}
-
 			}
 			fflush(stdin);
-			draw_ship(ship_x, ship_y);
 		}
+		//----------------------------------------------- input block -------------------------------------
+		//----------------------------------------------- drawing block -----------------------------------
+		draw_ship(ship_x, ship_y);
 		for (int i = 0; i < maxbullet; i++) {
-			if (bullet_shot[i] == true) {
-				bullet_shot[i] = draw_bullet(bullet_shot[i], bullet_x[i], bullet_y[i]);
+			if (bullet_shoot[i] == true) {
+				bullet_shoot[i] = draw_bullet(bullet_shoot[i], bullet_x[i], bullet_y[i]);
 				bullet_y[i]--;
 			}
 		}
+		//----------------------------------------------- drawing block -----------------------------------
 		Sleep(100);
+		//----------------------------------------------- erasing block -----------------------------------
+		erase_ship(ship_x, ship_y);
 		for (int i = 0; i < maxbullet; i++) {
 			erase_bullet(bullet_x[i], bullet_y[i] + 1);
 		}
-	
+		//----------------------------------------------- erasing block -----------------------------------
 	}
 
 	return 0;
 }
-//----------------------------------------------- Function
+
+//----------------------------------------------- Function ----------------------------------------------
 void draw_ship(int x, int y) {
 	gotoxy(x, y);
 	setcolor(2, 4);							// foreground 2::green , backgound 4::red
@@ -100,9 +111,9 @@ void erase_ship(int x, int y) {
 	setcolor(0, 0);
 	cout << "     ";
 }
-bool draw_bullet(bool bullet_shot, int x, int y) {
+bool draw_bullet(bool bullet_shoot, int x, int y) {
 	if (!over_frame(x, y)) {
-		if (bullet_shot) {
+		if (bullet_shoot) {
 			gotoxy(x + 2, y);
 			setcolor(7, 0);
 			cout << "i";
@@ -127,7 +138,6 @@ bool over_frame(int x, int y) {
 	}
 }
 
-
 void gotoxy(int x, int y) {
 	COORD c = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
@@ -143,3 +153,4 @@ void setcolor(int fg, int bg) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
 }
+//----------------------------------------------- Function ----------------------------------------------
