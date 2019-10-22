481cb78720cc617485c2fd7d50986dea4da61bdb
EsitoneX <phoom0529@gmail.com>
2019-10-22 22:07:19 +0700

    22/10/2562 22:06 windows update [ITC uploaded Ass6]

--- Semester1-1/Intro to computer engineering/Mini Project Clock/Mini_Project_Clock/Mini_Project_Clock.ino
+++ Semester1-1/Intro to computer engineering/Mini Project Clock/Mini_Project_Clock/Mini_Project_Clock.ino
@@ -1,12 +1,69 @@
-float timer1_start = 65473;     //second : 65535-62500 (+1) = 3035 (+1)   //millisecond : 65535-63 (+1) = 65472 (+1)
-int millisec = 0;
-int sec = 0;
-int minute = 0;
-int hour = 0;
+/////////////////////////// #define /////////////////////////////////
+#define BUTTON_MODE  3
+#define BUTTON_UP    7
+#define BUTTON_DOWN  8
+#define BUTTON_ENTER 9
+////////////////////////////////////////////////////////////////////
 
+/////////////////////////// Setup Variable /////////////////////////
+float timer1_start = 3036;            // second : 65535-62500 (+1) = 3035 (+1) , millisecond : 65535-63 (+1) = 65472 (+1)*/
+
+enum{
+  mode_setclock = 0,
+  mode_clock,
+  mode_stopwatch,
+  mode_countdown,
+};
+unsigned mode = mode_setclock;
+
+int pressed_timer;
+bool pressed_mode_button = false;
+
+struct Clock{
+  unsigned second;
+  unsigned minute;
+  unsigned hour;
+}clock;
+
+struct Stopwatch{
+  unsigned millisec;
+  unsigned second;
+  unsigned minute;
+}stopwatch;
+
+struct Countdown{
+  unsigned second;
+  unsigned minute;
+  unsigned hour;
+}countdown;
+////////////////////////////////////////////////////////////////////
+
+/////////////////////////// Interrupt service routine //////////////
+ISR(TIMER1_OVF_vect)                  //<-- Main Clock timer              
+{
+  TCNT1 = timer1_start;               // preload timer
+  Clock.second++;
+  if(clock.second==60){
+    clock.second = 0;
+    clock.minute++;
+  }
+  if(clock.minute==60){
+    clock.minute = 0;
+    clock.hour++;
+  }
+}
+////////////////////////////////////////////////////////////////////
+
+/////////////////////////// Main Programe //////////////////////////
 void setup(){
+  pinMode(BUTTON_MODE,INPUT_PULLUP);
+  pinMode(BUTTON_UP,INPUT_PULLUP);
+  pinMode(BUTTON_DOWN,INPUT_PULLUP);
+  pinMode(BUTTON_ENTER,INPUT_PULLUP);
+  
   Serial.begin(9600);
 
+  //--------- Timer 1 setup ----------------
   noInterrupts();
   TCCR1A = 0;
   TCCR1B = 0;
@@ -15,32 +72,120 @@ void setup(){
   TCCR1B = TCCR1B|(1 << CS12);  
   TIMSK1 = TIMSK1|(1 << TOIE1); 
   interrupts();
+  //----------------------------------------
 }
 
 void loop(){
-  Serial.print(hour);
-  Serial.print(" : ");
-  Serial.print(minute);
-  Serial.print(" : ");
-  Serial.print(sec);
-  Serial.print(" : ");
-  Serial.println(millisec); 
+  select_mode();
+
+  if(mode == mode_setclock){
+    setclock();
+  }
+  }
+  else if(mode == mode_clock){
+    show_clock();
+  }
+  else if(mode == mode_stopwatch){
+  }
+  else if(mode == mode_countdown){
+  }
+  
+}
+////////////////////////////////////////////////////////////////////
+
+/////////////////////////// User Define Function ///////////////////
+void select_mode(){
+  if(digitalRead(BUTTON_MODE)==LOW &&  pressed_mode_button == false){
+    pressed_timer = millis();
+    pressed_mode_button = true;
+    mode++;
+    if(mode == 4){
+      mode = 0;
+    }
+    
+    //----------------Show Mode-------------
+    if(mode == mode_setclock){
+      Serial.println("Mode_SetClock");
+    }
+    else if(mode == mode_clock){
+      Serial.println("Mode_Clock");
+    }
+    else if(mode == mode_stopwatch){
+      Serial.println("Mode_Stopwatch");
+    }
+    else if(mode == mode_countdown){
+      Serial.println("Mode_Countdown");
+    }
+    //---------------------------------------
+   }
+   delay(500);
+  if(millis()-pressed_timer>=500){
+    pressed_mode_button =false;
+    Serial.println(millis());
+  }
 }
 
-ISR(TIMER1_OVF_vect) // interrupt service routine
-{
-  TCNT1 = timer1_start;                   // preload timer
-  millisec++;
-  if(millisec==1000){
-    millisec = 0;
-    sec++;
+void show_clock(){
+  Serial.print(clock.hour);
+  Serial.print(" : ");
+  Serial.print(clock.minute);
+  Serial.print(" : ");
+  Serial.println(clock.second);
+}
+void show_setclock(){
+  Serial.print(setclock.hour);
+  Serial.print(" : ");
+  Serial.print(setclock.minute);
+  Serial.print(" : ");
+  Serial.println(setclock.second);
+}
+void show_stopwatch(){
+  Serial.print
+}
+void show_countdown(){
+  
+}
+
+void setclock(){
+  bool exit_mode = false;
+  
+  /*set clock only hour and minute*/
+  int setclock[2];
+  int setclock[0] = clock.hour;
+  int setclock[1] = clock.minute;
+
+  int i = 0;
+  while(!exit_mode){
+    if(digitalRead(BUTTON_MODE)==LOW){
+      exit_mode = true;
+    }
+    
+    if(digitalRead(BUTTON_UP)==LOW){
+      setclock[i]++;
+      if(setclock[0]>=24){
+        setclock[0] = 0;
+      }
+      if(setclock[1]>=60){
+        setclock[1] = 0;
+      }
+    }
+    if(digitalRead(BUTTON_DOWN)==LOW){
+      setclock[i]--;
+      if(setclock[0]<=0){
+        setclock[0] = 23;
+      }
+      if(setclock[1]<=0){
+        setclock[1] = 59;
+      }
+    }
+    
+    if(digitalRead(BUTTON_ENTER)==LOW){
+        i++;
+        if(i>=2){
+          i = 0;
+        } 
   }
-  if(sec==60){
-    sec = 0;
-    minute++;
-  }
-  if(minute==60){
-    minute = 0;
-    hour++;
-  }
+
+  clock.hour = setclock[0];
+  clock.minute = setclock[1];
 }
---
+++ Semester1-1/Intro to computer engineering/W11_15-10-2562/Assignment1_trafficlight/Assignment1_trafficlight.ino
@@ -0,0 +1,109 @@
+/////////////////////////////////////////////////////////// Define ////////////////////////////////////////////////////////////////////////////
+#define ROAD_S2N   2
+#define ROAD_W2E   3
+#define CROSSWALK  4
+
+#define LED_S_RED     5
+#define LED_S_YELLOW  6 
+#define LED_S_GREEN   7 
+
+#define LED_W_RED     8 
+#define LED_W_YELLOW  9 
+#define LED_W_GREEN   10
+
+#define LED_CROSSWALK_RED   11 
+#define LED_CROSSWALK_GREEN 12
+
+#define scango            0
+#define swaitgreen        1
+#define wcango            2
+#define wwaitgreen        3
+#define crossgo           4
+#define crosswaitgreenS   5
+#define crosswaitgreenW   6
+#define crossblink        7
+#define ROAD_S2N   2
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////// Variable //////////////////////////////////////////////////////////////////////////
+typedef struct State{
+  unsigned long output;
+  unsigned long wait;
+  unsigned long nextState[8];
+}state;
+
+state traffic[8]={
+  {B00110010  ,1000 ,{scango  ,crosswaitgreenS  ,wwaitgreen ,crosswaitgreenS  ,scango       ,crosswaitgreenS  ,wwaitgreen ,wwaitgreen}},
+  {B10001010  ,500  ,{scango  ,scango           ,scango     ,scango           ,scango       ,scango           ,scango     ,scango}},
+  {B10000110  ,1000 ,{wcango  ,crosswaitgreenW  ,wcango     ,crosswaitgreenW  ,swaitgreen   ,crosswaitgreenW  ,swaitgreen ,crosswaitgreenW}},
+  {B01010010  ,500  ,{wcango  ,wcango           ,wcango     ,wcango           ,wcango       ,wcango           ,wcango     ,wcango}},
+  {B10010001  ,1000 ,{crossgo ,crossgo          ,wcango     ,wcango           ,scango       ,scango           ,scango     ,scango}},
+  {B01010010  ,500  ,{crossgo ,crossgo          ,crossgo    ,crossgo          ,crossgo      ,crossgo          ,crossgo    ,crossgo}},
+  {B10001010  ,500  ,{crossgo ,crossgo          ,crossgo    ,crossgo          ,crossgo      ,crossgo          ,crossgo    ,crossgo}},
+};
+
+unsigned currentState = 0;
+unsigned previousState;
+
+unsigned input;                           // input 
+unsigned road_s2n ,road_w2e ,crosswalk;   // input       
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void setup(){
+  pinMode(ROAD_S2N,INPUT_PULLUP);
+  pinMode(ROAD_W2E,INPUT_PULLUP);
+  pinMode(CROSSWALK,INPUT_PULLUP);
+  
+  pinMode(LED_S_RED,OUTPUT);
+  pinMode(LED_S_YELLOW,OUTPUT);
+  pinMode(LED_S_GREEN,OUTPUT);
+
+  pinMode(LED_W_RED,OUTPUT);
+  pinMode(LED_W_YELLOW,OUTPUT);
+  pinMode(LED_W_GREEN,OUTPUT);
+
+  pinMode(LED_CROSSWALK_RED,OUTPUT);
+  pinMode(LED_CROSSWALK_GREEN,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop(){
+  if(previousState == crossgo && currentState != crossgo){
+    long timer;
+    for(int i=0;i<4;i++){
+      timer = millis();
+      digitalWrite(LED_CROSSWALK_GREEN,LOW);
+      while(millis()-timer<=100);{
+          // delay 100 ms 
+      }
+      timer = millis();
+      digitalWrite(LED_CROSSWALK_GREEN,HIGH);
+      while(millis()-timer<=100);{
+          // delay 100 ms
+      }
+    }
+  }
+  
+  digitalWrite(LED_S_RED, !(traffic[currentState].output & B10000000));
+  digitalWrite(LED_S_YELLOW, !(traffic[currentState].output & B01000000));
+  digitalWrite(LED_S_GREEN, !(traffic[currentState].output & B00100000));
+  
+  digitalWrite(LED_W_RED, !(traffic[currentState].output & B00010000));
+  digitalWrite(LED_W_YELLOW, !(traffic[currentState].output & B00001000));
+  digitalWrite(LED_W_GREEN, !(traffic[currentState].output & B00000100));
+  
+  digitalWrite(LED_CROSSWALK_RED, !(traffic[currentState].output & B00000010));
+  digitalWrite(LED_CROSSWALK_GREEN, !(traffic[currentState].output & B00000001));
+    
+  delay(traffic[currentState].wait);
+
+  road_s2n = digitalRead(ROAD_S2N);
+  road_w2e = digitalRead(ROAD_W2E);
+  crosswalk = digitalRead(CROSSWALK);
+
+  input = 7-(road_s2n*4 + road_w2e*2 + crosswalk);
+  
+  previousState = currentState;
+  currentState = traffic[currentState].nextState[input];
+}
