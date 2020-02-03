4907b3288c7a1ef1ab9ba949c729694598b46e97
EsitoneX <phoom0529@gmail.com>
2020-02-03 22:47:38 +0700

    3/2/2563 22:47 windows update [OOP updated week 3]

--- Semester1-2/Object Oriented Programming/HomeWork/HW3_3-2-2563/Course/TestCourse.java
+++ Semester1-2/Object Oriented Programming/HomeWork/HW3_3-2-2563/Course/TestCourse.java
@@ -1,19 +1,19 @@
 public class TestCourse{
     public static void main(String[] args){
         Course course = new Course("Object Oriented Programming");
-        System.out.println(course.toString() + "\n\n");
+        System.out.println(course.toString() + "\n");
         
         course.addStudent(new Student("62010001","A"));
         course.addStudent(new Student("62010002","B"));
         course.addStudent(new Student("62010003","C"));
         course.addStudent(new Student("62010004","D"));
         course.addStudent(new Student("62010005","E"));
-        System.out.println(course.toString() + "\n\n");
+        System.out.println(course.toString() + "\n");
         
         course.dropStudent("62010001","A");
         course.dropStudent("62010003","C");
         course.dropStudent("62010004","D");
-        System.out.println(course.toString() + "\n\n");
+        System.out.println(course.toString() + "\n");
 
         course.clear();
         System.out.println(course.toString());
