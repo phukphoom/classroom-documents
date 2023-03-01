d474f6841af8badcd79b77a79c9f8322bf72df7b
Phukphoom Taphrae <phoom0529@gmail.com>
2023-03-02 04:12:08 +0700

    2/3/2566 04:11 mac update [IDA uploaded]

--- Semester2-1/Data Structure and Algorithm/Lab/Lab7_18-9-2563/2.py
+++ Semester2-1/Data Structure and Algorithm/Lab/Lab7_18-9-2563/2.py
@@ -1,16 +1,16 @@
 ### Function ###
-def checkPalindome(string, head_index, tail_index) : 
-    if string[head_index] != string[tail_index] : 
+def checkPalindome(string, head_index, tail_index):
+    if string[head_index] != string[tail_index]:
         return False
-    else:
-        return True
 
-    if head_index < tail_index  : 
-        return checkPalindome(string, head_index + 1, tail_index - 1); 
+    if head_index < tail_index:
+        return checkPalindome(string, head_index + 1, tail_index - 1)
+
+    return True
 
 ### Main ###
 textInput = input("Enter Input : ")
-if checkPalindome(textInput,0,len(textInput)-1):
+if checkPalindome(textInput, 0, len(textInput)-1):
     print("'{}' is palindrome".format(textInput))
 else:
     print("'{}' is not palindrome".format(textInput))
